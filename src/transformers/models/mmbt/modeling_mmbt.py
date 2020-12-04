a9584ed657d5b6ca3cf50a1fad35256bed1e6983
mahaswec <mahaswec@usc.edu>
2020-12-04 23:11:54 +0530

    local changes

--- src/transformers/models/mmbt/modeling_mmbt.py
+++ src/transformers/models/mmbt/modeling_mmbt.py
@@ -342,7 +342,7 @@ class MMBTForClassification(nn.Module):
     def __init__(self, config, transformer, encoder):
         super().__init__()
         self.num_labels = config.num_labels
-
+        self.config = config
         self.mmbt = MMBTModel(config, transformer, encoder)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.classifier = nn.Linear(config.hidden_size, config.num_labels)
