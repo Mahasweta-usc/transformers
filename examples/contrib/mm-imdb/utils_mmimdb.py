e590df6762da8b872332a0982939cb5b5196dc1f
mahaswec <mahaswec@usc.edu>
2020-12-05 13:44:55 +0530

    init

--- examples/contrib/mm-imdb/utils_mmimdb.py
+++ examples/contrib/mm-imdb/utils_mmimdb.py
@@ -71,7 +71,7 @@ class JsonlDataset(Dataset):
             image = Image.open(os.path.join(self.data_dir, self.data[index]["img"])).convert("RGB")
         except Exception as e:
             print(e)
-            # image = Image.new("RGB", (600, 600), (255, 255, 255))
+            image = Image.new("RGB", (600, 600), (255, 255, 255))
         image = self.transforms(image)
 
         return {
