39a9b493b827cad7c42ddd58ce7ab25c630bea6d
mahaswec <mahaswec@usc.edu>
2020-12-06 04:53:48 +0530

    init

--- examples/contrib/mm-imdb/utils_mmimdb.py
+++ examples/contrib/mm-imdb/utils_mmimdb.py
@@ -52,11 +52,11 @@ class JsonlDataset(Dataset):
         
         if "train" in data_path:
             Data = [[json.loads(l)] for l in open(data_path)]
-            print(Data)
             oversample = RandomOverSampler(sampling_strategy='minority')
             temp_labels = [item[0]["label"] for item in Data]
             print(len(Data),len(temp_labels))
             self.data, _ = oversample.fit_resample(np.array(Data), temp_labels)
+            print(self.data)
             self.data = self.data.reshape(-1,1).tolist()
         else:
             self.data = [json.loads(l) for l in open(data_path)]
