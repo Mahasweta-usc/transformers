e47db161e5f34676b5697fb6637e8fc6e83974d2
mahaswec <mahaswec@usc.edu>
2020-12-10 04:19:09 +0530

    seg fault

--- src/transformers/models/mmbt/modeling_mmbt.py
+++ src/transformers/models/mmbt/modeling_mmbt.py
@@ -222,7 +222,7 @@ class MMBTModel(nn.Module, ModuleUtilsMixin):
         output_hidden_states = (
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
         )
-        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        return_dict = return_dict if return_dict is not None else self.config.return_dict
 
         if input_ids is not None and inputs_embeds is not None:
             raise ValueError("You cannot specify both input_ids and inputs_embeds at the same time")
